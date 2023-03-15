70dd2b9de4698dcdaddaf8dae9a8ba100ebfd8f5
y architect <64083403+beni20@users.noreply.github.com>
2023-03-15 13:40:09 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ const plugins = [
     resolve: "gatsby-source-filesystem",
     options: {
       name: "docs",
-      path: `${__dirname}/content/diary/nomad_diary/`
+      path: `${__dirname}/content/diary/tech_diary/`
     }
   },
   {
